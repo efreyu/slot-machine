7fa8a7cfa8d05a0c79e16650904a9734f576ba85
Artur Barilo <faustkeller45@gmail.com>
2019-07-07 19:34:01 +0300

    slot-machine | migrations & make auth

--- database/migrations/2019_07_07_163108_create_gift_table.php
+++ database/migrations/2019_07_07_163108_create_gift_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGiftTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('gifts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->double('balance');
-            $table->double('bonuses');
-            $table->rememberToken();
+            $table->integer('min_count')->default(1);
+            $table->integer('max_count')->default(1);
+            $table->string('comment')->nullable();
             $table->timestamps();
         });
     }
@@ -33,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('gifts');
     }
 }
--- database/migrations/2019_07_07_163139_create_item_gift_table.php
+++ database/migrations/2019_07_07_163139_create_item_gift_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateItemGiftTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('item_gift', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->double('balance');
-            $table->double('bonuses');
-            $table->rememberToken();
+            $table->unsignedInteger('item_id');
+            $table->unsignedInteger('gift_id');
             $table->timestamps();
+
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('restrict');
+            $table->foreign('gift_id')->references('id')->on('gifts')->onDelete('restrict');
         });
     }
 
@@ -33,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('item_gift');
     }
 }
